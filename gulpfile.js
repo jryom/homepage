4af374a60c322440ec1bad3788c50a468fb6720f
Jesper Ryom <jesper@ryom.me>
2020-03-03 10:55:43 +0100

    add gulp, purgecss and tachyons

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const purgecss = require('gulp-purgecss');
+const rename = require('gulp-rename');
+
+gulp.task('default', () => {
+  return gulp
+    .src('node_modules/tachyons/css/tachyons.min.css')
+    .pipe(rename('styles.css'))
+    .pipe(
+      purgecss({
+        content: ['index.html'],
+      })
+    )
+    .pipe(gulp.dest('.'));
+});

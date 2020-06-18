b912b84788ddce3547a0d0cb9289e2cfba133252
Jesper Ryom <jesper@ryom.me>
2020-06-18 09:49:19 +0200

    remove gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 const gulp = require("gulp");
 const purgecss = require("gulp-purgecss");
-const rename = require("gulp-rename");
 
 gulp.task("watch", () => gulp.watch("**/index.html", gulp.series("styles")));
 
 gulp.task("styles", () =>
   gulp
     .src("node_modules/tachyons/css/tachyons.min.css")
-    .pipe(rename("styles.css"))
     .pipe(
       purgecss({
         content: ["index.html"],
